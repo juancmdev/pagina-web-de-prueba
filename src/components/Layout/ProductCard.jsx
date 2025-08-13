c2b1c2533ccce439599e3f96f77a785c7e2d3c1b
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-08-13 16:47:55 -0500

    ProductCard component created

---
+++ src/components/Layout/ProductCard.jsx
@@ -0,0 +1,7 @@
+export default function ProductCard({ producto }) {
+  return (
+    <div className="border p-4 rounded-lg shadow-md">
+      {/* Aquí mostraremos los datos del producto */}
+    </div>
+  );
+}
--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,7 +1,21 @@
+import { useState, useEffect } from "react";
+
+
+  
+
 export default function Productos() {
+
+  const [productos, setProductos] = useState([]);
+useEffect(() => {
+  fetch('http://localhost:3001/apiProductos')
+  .then(response => response.json())
+  .then(data => setProductos(data))
+}, [])
+
   return (
     <>
       <h1 className="text-4xl">Productos</h1>
+      {productos.map(producto => <h2 key={producto.id} className="text-2xl">{producto.nombre}</h2>)}
     </>
   )
 }
